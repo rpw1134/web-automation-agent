496dacd4e51bb2089c89aafd2996866e4ca2b68e
rpw1134 <ryanw1134@gmail.com>
2025-12-02 12:35:03 -0500

    started working on playwright fundtions. need to implement page tracking and page switching for agent

--- agent-backend/src/agent_backend/classes/BrowserManager.py
+++ agent-backend/src/agent_backend/classes/BrowserManager.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from playwright.async_api import BrowserContext, Browser, Playwright, async_playwright
+from uuid import uuid4, UUID
 
 class BrowserManager:
     
@@ -7,7 +8,7 @@ class BrowserManager:
         # 1 per agent sessions. ID --> BrowserContext
         self._playwright : Playwright | None = None
         self._browser: Browser | None = None
-        self._contexts: Dict[int, BrowserContext]  = {}
+        self._contexts: Dict[UUID, BrowserContext]  = {}
     
     async def initialize(self):
         self._playwright = await async_playwright().start()
@@ -35,4 +36,14 @@ class BrowserManager:
         if self._browser is None:
             raise RuntimeError("Browser instance is not initialized.")
         return self._browser
+    
+    def get_browser_context_by_id(self, context_id: UUID) -> BrowserContext:
+        browser_context = self._contexts.get(context_id, None)
+        if browser_context is None:
+            raise KeyError(f"No browser context found for ID: {context_id}")
+        try:
+            _ = browser_context.pages
+        except Exception:
+            raise RuntimeError(f"Browser context for ID: {context_id} is no longer valid.")
+        return browser_context
     
--- agent-backend/src/agent_backend/tools/playwright_functions.py
+++ agent-backend/src/agent_backend/tools/playwright_functions.py
@@ -1,7 +1,23 @@
 from ..main import browser_manager
 from playwright.async_api import Browser
+from uuid import UUID
+from playwright.async_api import BrowserContext
 
-async def go_to_page(url: str):
+async def get_browser_context_by_id(context_id: UUID) -> BrowserContext:
+    """Retrieve a browser context by its ID."""
+    return browser_manager.get_browser_context_by_id(context_id)
+
+async def go_to_page(context_id: UUID, url: str):
     """Navigate to a page using the global browser instance."""
-    browser: Browser = browser_manager.browser
+    #TODO: store page metadata if needed
+    browser_context = await get_browser_context_by_id(context_id)
+    page = await browser_context.new_page()
+    await page.goto(url)
+    return page
+
+async def get_open_pages(context_id: UUID):
+    """Retrieve all open pages in a given browser context."""
+    browser_context = await get_browser_context_by_id(context_id)
+    return browser_context.pages
+
 
